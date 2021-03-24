f23f1393b46dce33c538ab2fbb81b5c10a92b981
alanji <yu1314yu>
2021-03-24 18:23:08 +0800

    数据库

--- App/model/chainBranchModel.go
+++ App/model/chainBranchModel.go
@@ -9,12 +9,12 @@ import (
 var connection = "db_ex_doctor"
 
 func SetChainBranchRelation() *xorm.Engine {
+
 	host := support.GetCfgString("LOCAL_HOST")
 	port := support.GetCfgInt("LOCAL_PORT")
 	username := support.GetCfgString("LOCAL_USER")
 	password := support.GetCfgString("LOCAL_PASS")
 	dbname := connection
-	fmt.Printf("====%v \n", host)
 	addr := fmt.Sprintf("%s:%d", host, port)
 	return support.OpenEngine(addr, username, password, dbname)
 }
--- App/model/model.go
+++ App/model/model.go
@@ -1,9 +1,25 @@
 package model
 
+import (
+	"fmt"
+	"github.com/go-xorm/xorm"
+	"head_server/support"
+)
+
 type Connection struct {
 	host     string
 	port     int
 	username string
 	password string
 	dbname   string
+}
+
+func MysqlConnection(conn Connection) *xorm.Engine {
+	host := support.GetCfgString("LOCAL_HOST")
+	port := support.GetCfgInt("LOCAL_PORT")
+	username := support.GetCfgString("LOCAL_USER")
+	password := support.GetCfgString("LOCAL_PASS")
+	dbname := connection
+	addr := fmt.Sprintf("%s:%d", host, port)
+	return support.OpenEngine(addr, username, password, dbname)
 }
