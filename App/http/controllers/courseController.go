1550ef040a2889bba3ecfb763665d54fd8d3e9fc
alanji <yu1314yu>
2021-03-25 16:11:10 +0800

    数据库

--- App/http/controllers/courseController.go
+++ App/http/controllers/courseController.go
@@ -6,35 +6,28 @@ import (
 	"net/http"
 )
 
-type Request struct {
-	AppId          string `json:"app_id"`
-	ShopName       string `json:"shop_name"`
-	ShopStatus     int    `json:"shop_status"`
-	ShopAuthStatus int    `json:"shop_auth_status"`
-	PageNum        int    `json:"page_num"`
-	PageSize       int    `json:"page_size"`
-}
-
 func DoctorList(c *gin.Context) {
+
+	type Request struct {
+		AppId          string `json:"app_id"`
+		ShopName       string `json:"shop_name"`
+		ShopStatus     int    `json:"shop_status"`
+		ShopAuthStatus int    `json:"shop_auth_status"`
+		PageNum        int    `json:"page_num"`
+		PageSize       int    `json:"page_size"`
+	}
 	var request Request
 	c.BindJSON(&request)
 
 	appId := request.AppId
-	shopName := request.ShopName
-	shopStatus := request.ShopName
-	shopAuthStatus := request.ShopAuthStatus
-	pageNum := request.PageNum
-	pageSize := request.PageSize
-
 	headAppId := model.GetDoctorList(appId)
 	c.JSON(http.StatusOK, gin.H{
-		"app_id":           appId,
-		"shop_name":        shopName,
-		"shop_status":      shopStatus,
-		"shop_auth_status": shopAuthStatus,
-		"page_num":         pageNum,
-		"page_size":        pageSize,
 		"head_app_id":      headAppId,
+		"shop_name":        request.ShopName,
+		"shop_status":      request.ShopName,
+		"shop_auth_status": request.ShopAuthStatus,
+		"page_num":         request.PageNum,
+		"page_size":        request.PageSize,
 	})
 	return
 }
